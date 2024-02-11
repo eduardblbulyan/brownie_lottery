22d2519034e832d941999f8333730533c6b7dbc3
Edward <edw.blb@gmail.com>
2024-02-11 21:23:22 +0400

    Adding contract Lotter

---
+++ contracts/Lottery.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.6.0;
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+
+
+contract Lottery {
+
+    address payable[] public players;
+    uint256 public usdEntryFee;
+    AggregatorV3Interface internal ethUsdPriceFeed;
+
+
+    constructor(address _priceFeedAddress) public {
+        usdEntryFee = 50 * (10**18); // because in Wei, convert to Eth;
+        ethUsdPriceFeed = AggregatorV3Interface();
+    }
+
+    function enter() public payable {
+        // 50$ minimum
+        require(msg.value >=)
+        players.push(msg.sender);
+    }
+    function get_entrance_fee() public view returns(uint256){
+        
+    }
+    function start_lotter() public {}
+    function end_lotter() public {}   
+}
